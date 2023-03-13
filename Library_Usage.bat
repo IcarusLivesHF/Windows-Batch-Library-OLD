ac761c6d05702bf1305e7c53e7a9145153bdd321
IcarusLivesHF <fg92sk8r@yahoo.com>
2023-03-13 18:55:30 -0400

    For Rev.3.29.3 or later

--- Library_Usage.bat
+++ Library_Usage.bat
@@ -1,12 +1,11 @@
 @echo off & setlocal enableDelayedExpansion
 
-set "revisionRequired=3.29.3"
+set "revisionRequired=3.29.2"
 set  "import(=(set "\i=?" & ren "%~nx0" -t.bat & ren "?.bat" "%~nx0""
 set ")=ren "%~nx0" "^^!\i^^!.bat" & ren -t.bat "%~nx0")" & set "self=%~nx0"
 set "failedLibrary=ren -t.bat "%~nx0" ^&echo  Missing Library. Required Revision:%revisionRequired% ^& timeout /t 3 ^& exit"
 (2>nul %import(:?=Library% && ( call :revision ) || ( %failedLibrary% ))
-	call :stdlib WID HEI fontSize /title "My Title" /color /rgb "0;255;0" "255;255;255"
-	call :ExtLib
+	call :stdlib /w:50 /h:20 /fs:12 /title:"My Title" /rgb:"255;255;255":"255;0;0" /c
 	call :math
 	call :misc
 	call :shapes
@@ -18,7 +17,9 @@ set "failedLibrary=ren -t.bat "%~nx0" ^&echo  Missing Library. Required Revision
 %)%  && (cls&goto :setup)
 :setup
 
+rem stdLib arguments can be in any order
+
 rem PLEASE NOTE: You do NOT have to call all of the functions above, you can call whichever ones you need to keep the variable environment low
 rem YOUR CODE GOES HERE
 
-pause 
+pause & exit
