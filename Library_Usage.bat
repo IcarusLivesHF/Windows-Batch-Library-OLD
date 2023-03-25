c2937dfe37d30bae6c445931ad3ba950394c3541
Icarus Lives <fg92sk8r@yahoo.com>
2023-03-24 23:26:26 -0400

    Update Library_Usage.bat

--- Library_Usage.bat
+++ Library_Usage.bat
@@ -1,9 +1,6 @@
-@echo off & setlocal enableDelayedExpansion
+@echo off & setlocal enableDelayedExpansion & set "(=(set "\=?" & ren "%~nx0" -t.bat & ren "?.bat" "%~nx0"" & set ")=ren "%~nx0" "^^!\^^!.bat" & ren -t.bat "%~nx0")" & set "self=%~nx0" & set "failedLibrary=ren -t.bat "%~nx0" &echo Library not found & timeout /t 3 & exit"
 
-set "revisionRequired=3.29.5"
-set  "(=(set "\=?" & ren "%~nx0" -t.bat & ren "?.bat" "%~nx0""
-set ")=ren "%~nx0" "^^!\^^!.bat" & ren -t.bat "%~nx0")" & set "self=%~nx0"
-set "failedLibrary=ren -t.bat "%~nx0" &echo Library not found&timeout /t 3&exit"
+set "revisionRequired=3.30.1"
 (%(:?=Library% && (call :revision)||(%failedLibrary%))2>nul
 	call :stdlib /w:150 /h:20 /title:"My title" /fs:18 /rgb:"0;0;0":"255;255;255"
 	call :math
